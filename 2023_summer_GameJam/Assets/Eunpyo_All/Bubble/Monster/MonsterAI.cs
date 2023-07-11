2e9e3748bd01fd38876fdf729e375a16ed39af94
PyoDev <shouns0621@gmail.com>
2023-07-11 21:22:23 +0900

    dwq

--- 2023_summer_GameJam/Assets/Eunpyo_All/Bubble/Monster/MonsterAI.cs
+++ 2023_summer_GameJam/Assets/Eunpyo_All/Bubble/Monster/MonsterAI.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class MonsterAI : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Rigidbody2D rb;
+    bool LR;
     void Start()
     {
-        
+        LR = true;
+        rb = GetComponent<Rigidbody2D>();
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (LR)
+        {
+            Vector3 raypos = new Vector3(rb.position.x - 0.7f, rb.position.y, 0);
+            Debug.DrawRay(raypos, Vector3.down, new Color(1, 0, 0));
+            RaycastHit2D raycastHit = Physics2D.Raycast(raypos, Vector3.down, 0.5f);
+        }
     }
 }
