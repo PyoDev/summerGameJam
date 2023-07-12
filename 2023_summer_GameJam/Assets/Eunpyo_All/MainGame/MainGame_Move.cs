47813b037568fe85ae04a8064131b6b8f1c61209
PyoDev <shouns0621@gmail.com>
2023-07-12 09:30:36 +0900

    ju

--- 2023_summer_GameJam/Assets/Eunpyo_All/MainGame/MainGame_Move.cs
+++ 2023_summer_GameJam/Assets/Eunpyo_All/MainGame/MainGame_Move.cs
@@ -6,16 +6,22 @@ using UnityEngine;
 public class MainGame_Move : MonoBehaviour
 {
     Rigidbody2D rb;
+    float time;
     public float speed;
     public float JumpPoewr;
     public static bool Jump;
     private void Awake()
     {
+        time = 0.0f;
         rb = GetComponent<Rigidbody2D>();
         Jump = false;
     }
     void Update()
     {
+        if(Jump)
+        {
+            time += Time.deltaTime;
+        }
         rb.velocity = new Vector2(speed,rb.velocity.y);
         this.gameObject.transform.position += new Vector3(speed,0,0) * Time.deltaTime;
         if (Input.GetKeyDown(KeyCode.Space) && Jump == false)
@@ -23,6 +29,20 @@ public class MainGame_Move : MonoBehaviour
             rb.AddForce(Vector3.up * JumpPoewr, ForceMode2D.Impulse);
             Jump = true;
         }
-        
+        int layMask = (-1) - (1 << LayerMask.NameToLayer("UI"));
+        Debug.DrawRay(rb.position, new Vector3(0,-0.3f,0), new Color(0, 1, 0));
+        RaycastHit2D raycastHit = Physics2D.Raycast(rb.position, new Vector3(0, -0.3f, 0), 0.6f, layMask);
+        if (raycastHit.collider != null)
+        {
+            if(raycastHit.transform.gameObject.tag == "Round")
+            {
+                Debug.Log(raycastHit.transform.gameObject.tag);
+                if(time > 0.3f)
+                {
+                    Jump = false;
+                    time = 0.0f;
+                }
+            }
+        }
     }
 }
